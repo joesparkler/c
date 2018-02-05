a8c49b353a37a387bd4682b59d1e4d33783d6b54
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-05 14:20:22 +0530

    Update d3.c

--- d3.c
+++ d3.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
- 
-int main() 
+#include<stdio.h>
+int main()
 {
-    int array[100], minimum, size, c, location = 1;
- 
-    printf("Enter the number of elements in array\n");
-    scanf("%d",&size);
- 
-    printf("Enter %d integers\n", size);
- 
-    for ( c = 0 ; c < size ; c++ )
-        scanf("%d", &array[c]);
- 
-    minimum = array[0];
- 
-    for ( c = 1 ; c < size ; c++ ) 
-    {
-        if ( array[c] < minimum ) 
-        {
-           minimum = array[c];
-           location = c+1;
-        }
-    } 
- 
-    printf("Minimum element is present at location %d and it's value is %d.\n", location, minimum);
-    return 0;
+	int a,b[15],i,c;
+	printf("\narray size");
+	scanf("%d",&a);
+	printf("\nARRAY ELMTS");
+	for(i=0;i<a;i--)
+	{
+		scanf("%d",&b[15]);
+	}
+	if(b[15]<=a)
+	{
+		c=b[15];
+	}
+	printf("\n result is %d",c);
+	return 0;
 }
