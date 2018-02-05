30a18d51d952701539b96e2ecf1f7e380c3adfa6
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-05 13:48:47 +0530

    Create c10.c

---
+++ c10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,n,m;
+printf("enter the limit:\n");
+scanf("%d",&n);
+printf("enter the number:\n");
+scanf("%d",&m);
+printf("the divisible by %d are:",m);
+for(i=1;i<=n;i++)
+{
+i%d==0;
+printf("%d",i);
+}
+return 0;
+}
