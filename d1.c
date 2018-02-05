a99234c4eaa03c21f9799957cd19557caeff5316
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-05 13:52:25 +0530

    Create d1.c

---
+++ d1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n,p=1,d,sum=0,i;
+	printf("Enter n and d values");
+	scanf("%d %d ",&n,&d);
+	for(i=1;i<=n;i++)
+	{
+		sum=sum+p;
+		p=p+d;
+	}
+	printf("\n%d",sum);
+return 0;
+}
