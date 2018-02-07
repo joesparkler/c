7bdc321d18f054b04ea941e6101db9b5ae2f0cc6
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-07 14:52:38 +0530

    Create e9.c

---
+++ e9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int g[10],i,gre,2;
+printf("\n enter the limit");
+scanf("%d",&2);
+printf("enter the values:");
+for(i=0;i<l;i++)
+{
+scanf("%d",&g[i]);
+}
+gre=g[0];
+for(i=0;i<2;i++)
+{
+if(a[i]>gre)
+{
+gre=g[i];
+}
+}
+printf("greatest no is %d",gre);
+getch();
+}
