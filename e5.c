6166109b00189a17f5f333a3ddc022ca63565330
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-07 14:41:54 +0530

    Create e5.c

---
+++ e5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i,j=0,n;
+ char a[50]="sarani is good.234";
+ for(i=0;a[i]!='\0';i++)
+ {
+  if(a[i]>='0'&&a[i]<='9')
+  {
+  c++;
+  }
+  }
+   printf("%d",j);
+   }
