2d23b5479c15d1917bf64d47027fcb0c2114852d
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-07 14:09:22 +0530

    Update d7.c

--- d7.c
+++ d7.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 main()
 {
-	char ch;
-	scanf("%c",&ch);
-	if(ch=='0'||ch=='1'||ch=='2'||ch=='3'||ch=='4'||ch=='5'||ch=='6'||ch=='7'||ch=='8'||ch=='9')
+	char ab;
+	scanf("%c",&ab);
+	if(ab=='0'||ab=='1'||ab=='2'||ab=='3'||ab=='4'||ab=='5'||ab=='6'||ab=='7'||ab=='8'||ab=='9')
 	{
 		printf("yes");
 
