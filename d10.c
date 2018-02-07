fd9b385373a674f63f3c9cadea924c38ea54e772
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-07 14:26:41 +0530

    Update d10.c

--- d10.c
+++ d10.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
-void main()
+int main()
 {
-    int hr1,min1,hr2,min2,hr,min;
-    scanf("%d %d",&hr1,&min1);
-    scanf("%d %d",&hr2,&min2);
-    hr=hr1-hr2;
-    min=min1-min2;
-    printf("%d %d",hr,min);
+int hrs1,hrs2,mins1,mins2,m,n;
+printf("Enter the hrs1 and mins1:");
+scanf("%d%d",&hrs,&mins);
+printf("Enter the hrs2 and mins2:");
+scanf("%d%d",&hrs,&mins);
+m=hrs1-hrs2;
+n=mins1-mins2;
+printf("%d %d",m,n);
+return 0;
 }
