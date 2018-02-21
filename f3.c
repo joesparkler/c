76ba229cf137253a970de40da463b1ee762ca694
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-21 14:07:51 +0530

    Create f3.c

---
+++ f3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+char s[20],s1[70];
+int a=0,b=o;
+printf("enter first string");
+gets(s);
+printf("enter second string");
+gets(s1);
+while(s[a]!='\0')
+a++;
+while(s1[b]!='\0')
+{
+s=s1;
+b++;
+a++;
+}
+s='\0';
+printf("%s",&s);
+}
