dac5b99c9ec42701a3f133028b5873b3c26d5280
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-21 14:26:08 +0530

    Create f8.c

---
+++ f8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main() 
+{
+int a,b,sum=0,num;
+ 
+  printf(" Enter How many Number you want");
+  scanf("%d",&b);
+ 
+  printf(" Enter the elements one by one");
+  for(i=0;i<b;++i)
+   {
+     scanf("%d",&num);
+     Sum = Sum +num;
+ 
+  return 0;
+}
