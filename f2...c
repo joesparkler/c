3dd8958f9b970e475780e38d87d6c6055df0c2bd
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-21 14:00:58 +0530

    Create f2...c

---
+++ f2...c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char string1=6,string2=5;
+int i,temp;
+printf("enter the string1 and string2 value");
+fot(i=0;string1(i)!'\0';i++)
+{
+if(string1==string2)
+{
+temp=0;
+else
+temp=1;
+}
+if(temp==1)
+{
+printf("both strinr are same');
+else
+printf("both string are not same")
+}
+}
