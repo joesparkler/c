674bfc8191754cbfcc186783ea0a07e861dce8ac
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-26 13:46:49 +0530

    Create h2.c

---
+++ h2.c
@@ -0,0 +1,18 @@
+s (17 sloc)  222 Bytes
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int str;
+int i;
+printf("enter the string");
+scanf("%d",&str);
+if((str==0)||(str==1))
+{
+printf("binary representation");
+}
+else
+{
+printf("not a binary representation");
+}
+}
