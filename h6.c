d9dea4f39f78201b65afe6d3ac5c8f5f32d9835d
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-02-28 14:07:28 +0530

    Create h6.c

---
+++ h6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ void main()
+ {
+	int n,i,count=0;
+  printf("Enter the number");
+  scanf("%d",&n);
+	for(i=2;i<=n;i++)
+	{
+		if(n%i==0)
+		count++;
+	}
+	if(count<3)
+	{
+		printf("prime number");
+	}
+	else
+	{
+		printf("not a prime number");
+	}
+}
