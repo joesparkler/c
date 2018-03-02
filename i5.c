04e66e129fba9bfc3f8c1d4d09f260dd0ce1f835
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-03-02 15:53:06 +0530

    Create i5.c

---
+++ i5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int count=0,i,d;
+char str[100]="aashika",c='*';
+/*printf("enter the string");
+  scanf("%c",&str);*/
+  for(i=0;str[i]!='\0';i++)
+  {
+      count++;
+  }
+printf("%d",count);
+d=count/2;
+if(d%2==0)
+{
+    str[d]=c;
+    str[d+1]=c;
+    for(i=0;str[i]!='\0';i++)
+    printf("%c",str[i]);
+    
+}
+else
+{
+    str[d]=c;
+    for(i=0;str[i]!='\0';i++)
+    printf("%c",str[i]);
+}
+return 0;
+}
