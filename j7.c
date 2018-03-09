e85ce333963bf30cb978ecb3e3cb80a8349c7192
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-03-09 15:52:34 +0530

    Create j7.c

---
+++ j7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,m,res,p,gcd,i;
+    scanf("%d %d",&n,&m);
+    for(i=1;i<=n&&i<=m;i++)
+    {
+        if(n%i==0&&m%i==0)
+        {
+            gcd=i;
+        }
+    }
+    
+    printf("%d",gcd);
+    }
