edbb0eb991c780a169403046bee805e177a46f52
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-03-14 16:00:16 +0530

    Create k5.c

---
+++ k5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,p,t;
+float r;
+printf("enter the principal amount");
+scanf("%d",&p);
+printf("enter the time period");
+scanf("%d",&t);
+printf("enter the rate");
+scanf("%f",&r);
+a=p *(1+r*t);
+printf("%d",a);
+return0;
+}
