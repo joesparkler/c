6449a4ba6f611b1e56b131e7db5eec6a0f66d33e
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-03-16 15:38:46 +0530

    Create k6.c

---
+++ k6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+    int n,i,temp=0;
+ 
+    scanf("%d",&n);
+ 
+    for(i=2; i<=n/2; ++i)
+    {
+ 
+        if(n%i==0)
+        {
+            temp++;
+            break;
+        }
+    }
+ 
+    if (temp==0)
+        printf("no");
+    else
+        printf("yes");
+}
