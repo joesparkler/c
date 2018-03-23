123532cb4b28ec03e387fbd86d52c07d4ece9db9
joesparkler <35687869+joesparkler@users.noreply.github.com>
2018-03-23 16:32:19 +0530

    Create l9.c

---
+++ l9.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,j;
+clrscr();
+printf("Enter the array values:");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<10;i++)
+{
+for(j=i+1;j<10;j++)
+{
+if(a[i]>a[j])
+{
+a[i]=a[i]+a[j];
+a[j]=a[i]-a[j];
+a[i]=a[i]-a[j];
+}
+}
+}
+printf("The minimum element is:%d",a[i]);
+getch();
+}
